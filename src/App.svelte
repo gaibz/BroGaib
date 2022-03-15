ace258c0ba020b21d7b0a96c30f7e46fbe0611d1
Herlangga Sefani <herlangga.sefani@gmail.com>
2022-03-15 16:47:13 +0700

    That's it for today .. css stuff is really confusing

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,8 @@
-<h1>Hello World</h1>
+<script>
+    import Sidebar from "./components/Sidebar.svelte"
+</script>
+<Sidebar />
+
+<div class="main">
+    <h1>Hello World</h1>
+</div>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,5 @@
+<div class="sidenav">
+    {#each Array(10) as _, i}
+        <a href="#">Tab {i}</a>
+    {/each}
+</div>
